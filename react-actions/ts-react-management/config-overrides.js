60bd117fc6c33f63c4649780ade4ffa60f1c3061
dengwb <dengwenbin@wecash.net>
2019-11-07 16:20:51 +0800

    add

--- react-actions/ts-react-management/config-overrides.js
+++ react-actions/ts-react-management/config-overrides.js
@@ -1,7 +1,11 @@
 // According to the need to load by Antd
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
+const path = require('path')
 
 module.exports = override(
+  addWebpackAlias({        
+    ['@assets']: path.resolve(__dirname, 'src/assets')       
+  }),
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
