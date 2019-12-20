c3dd23b89088aabe8b5c73c891cda972b1a14eb4
dengwb <dengwenbin@wecash.net>
2019-12-20 15:03:49 +0800

    typo

--- base-typescript/fourth-typescript/build/webpack.config.js
+++ base-typescript/fourth-typescript/build/webpack.config.js
@@ -3,6 +3,9 @@ const baseConfig = require('./webpack.base.config')
 const devConfig = require('./webpack.dev.config')
 const prodConfig = require('./webpack.prod.config')
 
-let config = process.NODE_ENV === 'development' ? devConfig : prodConfig
+module.exports = (env, argv) => {
+  // process.env.NODE_ENV
+  const config = argv.mode === 'development' ? devConfig : prodConfig
+  return merge(baseConfig, config)
+}
 
-module.exports = merge(baseConfig, config)
--- base-typescript/third-typescript/build/webpack.config.js
+++ base-typescript/third-typescript/build/webpack.config.js
@@ -3,6 +3,8 @@ const baseConfig = require('./webpack.base.config')
 const devConfig = require('./webpack.dev.config')
 const prodConfig = require('./webpack.prod.config')
 
-let config = process.NODE_ENV === 'development' ? devConfig : prodConfig
-
-module.exports = merge(baseConfig, config)
+module.exports = (env, argv) => {
+  // process.env.NODE_ENV
+  const config = argv.mode === 'development' ? devConfig : prodConfig
+  return merge(baseConfig, config)
+}
